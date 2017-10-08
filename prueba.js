694c6a9f02d206206f8e9ba1e83e5a31b58900ad
Sebas <seplarui@gmail.com>
2017-10-09 00:05:35 +0200

    search form

--- prueba.js
+++ prueba.js
@@ -2,10 +2,23 @@ casper= require("casper").create();
 
 casper.start("http://www.google.es");
 
+casper.viewport(1200,720);
+
 casper.then(function() {
     casper.echo("Pagina cargada con exito");
     //captura pantalla
-    casper.capture("google.png");
+    //casper.capture("google.png");
+
+
+    casper.fill('form[action="/search"]', {
+        q: 'casperjs'
+
+    }, true);
+
+});
+
+casper.then(function(){
+    casper.capture("google.png",{top:0, left:0, width:1200, height:720});
 });
 
 
